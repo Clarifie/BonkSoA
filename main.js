519a6e77a59e26c4c8f818e8b521504d8169dab3
Clarifie <friendlyneighborhoodemailbot1@gmail.com>
2023-12-19 16:08:59 -0500

    Testing three.js

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-console.log(THREE);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xcccccc);
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 2000);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+
+const loader = new GLTFLoader();
+loader.load(
+    'data/SoA1-0.glb',
+    function(gltf) {
+        scene.add(gltf.scene);
+        gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+    },
+    function(xhr) {
+        console.log(( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+	},
+    function(error) {
+        console.log('ERROR');
+    }
+);
+
+/*const cube = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshBasicMaterial({color: 0x00ff00}));
+scene.add(cube);*/
+
+camera.position.set(0, 20, 50);
+controls.update();
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+animate();
