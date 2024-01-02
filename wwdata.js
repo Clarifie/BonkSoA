eb9aa8141437218d554b130eef5dfd64b2df929d
Clarifie <friendlyneighborhoodemailbot1@gmail.com>
2024-01-02 09:32:22 -0500

    world record backend ideas

---
+++ wwdata.js
@@ -0,0 +1,20 @@
+//todo
+/**
+ * Get map data from either map....php from bonk or get
+ * it from lbb db
+ * 
+ * Initiallu sort from recent world records, allows a search,
+ * need to prevent searching more than once per second and
+ * prevent changing the limit
+ * 
+ * on click of a map, take map data and create an svg from
+ * the data, this might or might not happen depending on
+ * how slow it takes to create, if it is fast this could
+ * be done for all the maps shown.
+ * 
+ * figure out how to get the preview image of a map that
+ * bonk has in its search
+ * 
+ * make css for: card -> image, title, record time
+ * possibly add description or expandable description?
+ */
